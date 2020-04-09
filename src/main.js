d4fe935948b2374395ea6fe00bdc3b3c93f9066b
Sergey <10rubleqow@gmail.com>
2020-04-09 15:17:33 +0300

    Отрефакторил вызовы функции render в main.js

--- src/main.js
+++ src/main.js
@@ -33,9 +33,9 @@ render(taskListElement, createTaskEditTemplate(tasks[0]));
 let showingTasksCount = SHOWING_TASKS_COUNT_ON_START;
 
 tasks.slice(1, showingTasksCount)
-  .forEach((task) => render(taskListElement, createTaskTemplate(task), `beforeend`));
+  .forEach((task) => render(taskListElement, createTaskTemplate(task)));
 
-render(boardElement, createLoadMoreButtonTemplate(), `beforeend`);
+render(boardElement, createLoadMoreButtonTemplate());
 
 const loadMoreButton = boardElement.querySelector(`.load-more`);
 
@@ -44,7 +44,7 @@ loadMoreButton.addEventListener(`click`, () => {
   showingTasksCount = showingTasksCount + SHOWING_TASKS_COUNT_BY_BUTTON;
 
   tasks.slice(prevTasksCount, showingTasksCount)
-    .forEach((task) => render(taskListElement, createTaskTemplate(task), `beforeend`));
+    .forEach((task) => render(taskListElement, createTaskTemplate(task)));
 
   if (showingTasksCount >= tasks.length) {
     loadMoreButton.remove();
