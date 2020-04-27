b382dd1bf363a019b103fdff3c0a69cd28120e23
Sergey <10rubleqow@gmail.com>
2020-04-28 00:50:40 +0300

    Выполнил основное домашнее задание по учебному проекту

--- src/const.js
+++ src/const.js
@@ -1,4 +1,12 @@
-const COLORS = [`black`, `yellow`, `blue`, `green`, `pink`];
+const COLOR = {
+  BLACK: `black`,
+  YELLOW: `yellow`,
+  BLUE: `blue`,
+  GREEN: `green`,
+  PINK: `pink`,
+};
+
+const COLORS = Object.values(COLOR);
 
 const DAYS = [`mo`, `tu`, `we`, `th`, `fr`, `sa`, `su`];
 
@@ -30,4 +38,13 @@ const filters = {
   archive: 0
 };
 
-export {COLORS, DAYS, MONTH_NAMES, FILTER_NAMES, filters};
+const FilterType = {
+  ALL: `all`,
+  ARCHIVE: `archive`,
+  FAVORITES: `favorites`,
+  OVERDUE: `overdue`,
+  REPEATING: `repeating`,
+  TODAY: `today`,
+};
+
+export {COLOR, COLORS, DAYS, MONTH_NAMES, FILTER_NAMES, filters, FilterType};
